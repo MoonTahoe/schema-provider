8d0000e5601731206c97cbb65bc8ebe9ca39ea8f
MoonTahoe <alex@moonhighway.com>
2018-11-20 16:06:54 -0800

    Adding more user fields to test schema diff

--- src/mocks/Mutation.js
+++ src/mocks/Mutation.js
@@ -2,5 +2,6 @@ import { MockList } from 'apollo-server'
 import { currentUser } from './currentUser'
 
 export const Mutation = () => ({
-  register: (_, { email, password }) => currentUser
+  register: (_, { email, password }) => currentUser,
+  verifyAccount: (_, { email, password }) => currentUser
 })
